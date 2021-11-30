00305254b4bfafa7d755e4b92b19a452ece035e6
Sam <samuelnunez@cpp.edu>
2021-11-29 23:48:16 -0800

    Added addEdge and BFS methods

--- src/Graph.java
+++ src/Graph.java
@@ -15,5 +15,41 @@ public class Graph extends Main {
             for (int i=0; i<v; ++i)
                 adj[i] = new LinkedList();
         }
+        // adds edge
+        void addEdge(int v,int w)
+        {
+            adj[v].add(w);
+        }
 
+        // prints from source s
+        void BFS(int s)
+        {
+            // Marks visited vertices
+            boolean visited[] = new boolean[V];
+
+            //create q
+            LinkedList<Integer> queue = new LinkedList<Integer>();
+
+            // mark the actual visited vert
+            visited[s]=true;
+            queue.add(s);
+
+            while (queue.size() != 0)
+            {
+                // Dequeue and print
+                s = queue.poll();
+
+                System.out.print((char)(s + 65) + " ");
+                Iterator<Integer> i = adj[s].listIterator();
+                while (i.hasNext())
+                {
+                    int n = i.next();
+                    if (!visited[n])
+                    {
+                        visited[n] = true;
+                        queue.add(n);
+                    }
+                }
+            }
+        }
 }
