ab61ddc03d799bdf7581b6744bd3c00e0cddcf0a
Sam <samuelnunez@cpp.edu>
2021-11-29 23:57:11 -0800

    added node

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
 
-public class Node {
+class Node implements Comparable{
+        private char label;
+        ArrayList<Node> pointers = new ArrayList<Node>();
+        int status = 0;
+        //0 ready
+        //1 waiting
+        //2 processed
 
-}
+
+        public Node(char label) {
+            this.label = label;
+        }
+
+        public void addPointer(Node child) {
+            pointers.add(child);
+        }
+        public char getLabel() {
+            return label;
+        }
+        public void printNode() {
+            for(Node pointer: pointers) {
+                System.out.println("(" + label + ", " + pointer.getLabel() + ")");
+            }
+        }
+
+        @Override
+        public int compareTo(Object arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+    }
