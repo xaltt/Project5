8a3c56a947b948f608344d8460f7e58bf661b74e
Sam <samuelnunez@cpp.edu>
2021-11-29 23:49:31 -0800

    Added graph2 class

---
+++ src/Graph2.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+// This class implements the Depth First Traversal
+class Graph2 {
+    private int V; // No. of vertices
+
+    // Array of lists
+    private LinkedList<Integer> adj[];
+
+
+}
