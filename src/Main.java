d3cdd6f93bd61ca264e20ac0a46f811f2e5e7011
Sam <samuelnunez@cpp.edu>
2021-11-29 23:53:42 -0800

    created object

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 
 	public class Main {
 	    public static void main(String[]args) {
+	    	//This is for Graph.java
+	        Graph g = new Graph(9);
+	        int vert = 0;
 	        
 	        
 
